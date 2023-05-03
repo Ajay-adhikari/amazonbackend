6f635982879bd51d43eadd8d92f7f1fe5c404a84
unknown <ajauadhi23@gmail.com>
2023-05-03 12:57:41 +0530

    change

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ app.use(cors());
 app.use(router);
 const port=8005;
 // console.log(process.env.DATABASE);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.listen(port,()=>{
     console.log(`server is runnning on port ${port}`);
 });
