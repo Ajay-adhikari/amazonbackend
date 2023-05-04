62512d6e3e3e6dcf37cd1f65fbab28e48bf8b66f
unknown <ajauadhi23@gmail.com>
2023-05-04 19:57:43 +0530

    changes

--- app.js
+++ app.js
@@ -9,25 +9,25 @@ const Products=require("./models/productSchema");
 const DefaultData=require("./defaultdata");
 const cors=require("cors");
 const router=require("./routes/router");
-const cookieParser = require("cookie-parser");
 
 app.use(express.json());
-app.use(cookieParser(""));
+app.use(CookieParser());
 app.use(cors());
 app.use(router);
-const port=8005;
-const corsConfig = {
-    credentials: true,
-    origin: true,
-};
-app.use(cors(corsConfig));
-// console.log(process.env.DATABASE);
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+const port=process.env.PORT || 8005;
+
+// const corsConfig = {
+//     credentials: true,
+//     origin: true,
+// };
+// app.use(cors(corsConfig));
+// // console.log(process.env.DATABASE);
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 app.listen(port,()=>{
     console.log(`server is runnning on port ${port}`);
 });
