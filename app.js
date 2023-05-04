fc5fe5e49d13419bf1a8cfac9fa852870e936a84
unknown <ajauadhi23@gmail.com>
2023-05-04 20:47:46 +0530

    chanje

--- app.js
+++ app.js
@@ -13,17 +13,15 @@ const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cookieParser(""));
-app.use(cors());
+// app.use(cors());
 app.use(router);
 const port=process.env.PORT || 8005;
-const corsConfig = {
-    credentials: true,
-    origin: true,
-};
-app.use(cors(corsConfig));
+
+// app.use(cors(corsConfig));
 // console.log(process.env.DATABASE);
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://localhost:3000");
+    
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
