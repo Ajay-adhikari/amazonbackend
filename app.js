3e8ea2191826b8f43a2b940cf950377d6a512e2e
unknown <ajauadhi23@gmail.com>
2023-05-05 21:01:45 +0530

    change

--- app.js
+++ app.js
@@ -23,9 +23,10 @@ app.use(router);
 
 const port=process.env.PORT || 8005;
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
 
