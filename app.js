4dbe7cfa944812e1ed54e8622157fac33da6437c
unknown <ajauadhi23@gmail.com>
2023-05-06 12:35:23 +0530

    change

--- app.js
+++ app.js
@@ -13,18 +13,13 @@ const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cookieParser(""));
+app.use(cors());
 
 app.use(router);
 
 
-const port=process.env.PORT || 8005;
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://gleeful-toffee-40719d.netlify.app');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
+const port= 8005;
+
 
 
 
