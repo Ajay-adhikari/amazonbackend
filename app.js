ec3885fc5b4d7496f88fdcbfbc36f7429d471458
unknown <ajauadhi23@gmail.com>
2023-05-06 16:20:11 +0530

    change

--- app.js
+++ app.js
@@ -13,7 +13,19 @@ const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cookieParser(""));
-app.use(cors());
+// app.use(cors());
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    credentials: true
+  };
+  
+  app.use(cors(corsOptions));
 
 app.use(router);
 
