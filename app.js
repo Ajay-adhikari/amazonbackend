e39ad14aecab20dc9fad51fb2196b7d7d5177c92
unknown <ajauadhi23@gmail.com>
2023-05-06 16:21:05 +0530

    change

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cookieParser(""));
 // app.use(cors());
+const cors = require('cors');
+const whitelist = ['https://stellar-treacle-3bd297.netlify.app'];
 const corsOptions = {
     origin: function (origin, callback) {
       if (whitelist.indexOf(origin) !== -1) {
