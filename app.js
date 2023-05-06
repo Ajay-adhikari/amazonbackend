44cb5b5239cd74d357c506dd33b752d9533eec69
unknown <ajauadhi23@gmail.com>
2023-05-06 17:03:45 +0530

    change

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cookieParser(""));
-app.use(cors());
+app.use(cors({
+    origin: 'https://bucolic-crumble-3245a8.netlify.app'
+  }));
 
 
 app.use(router);
