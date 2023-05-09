525847f82ae9e09f15b0fd4205473ecef3325dbb
unknown <ajauadhi23@gmail.com>
2023-05-09 20:30:40 +0530

    firse

--- routes/router.js
+++ routes/router.js
@@ -97,8 +97,7 @@ router.post("/login", async (req, res) => {
         path: '/',
         expires: new Date(Date.now() + 86400000), // 24 hours from now
         httpOnly: true,
-        secure: false,
-        
+        secure: true
       });
       res.status(201).json(userlogin);
     }}
