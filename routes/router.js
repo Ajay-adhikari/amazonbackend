aaa78f80b17c5f6814e57397ac12d7ad5927c211
unknown <ajauadhi23@gmail.com>
2023-05-09 20:18:27 +0530

    firse

--- routes/router.js
+++ routes/router.js
@@ -98,7 +98,7 @@ router.post("/login", async (req, res) => {
         expires: new Date(Date.now() + 86400000), // 24 hours from now
         httpOnly: true,
         secure: false,
-        domain: '.netlify.app'
+        
       });
       res.status(201).json(userlogin);
     }}
